7fdb8246420035a1eb02fe941c2e77ea17b2ff6a
Nisarg <154842959+Nisargkumar-Patel@users.noreply.github.com>
2025-02-06 09:19:36 -0500

    navbar icon

--- app/src/Components/Navbar.tsx
+++ app/src/Components/Navbar.tsx
@@ -5,7 +5,7 @@ function NavBar() {
     <div className="nav">
       <ul>
         <li className="li">
-          <p><img className="img" src="https://cdn-icons-png.flaticon.com/512/7133/7133364.png"></img></p>
+        <img className="img" src="https://cdn-icons-png.flaticon.com/512/7133/7133364.png" alt="weather"></img>
         </li>
         <li className="li">
           <p>Weather App</p>
